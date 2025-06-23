63ba3d988bc8ac44e5d707ae80fb7e3aee0efbf0
00090121-Auxi <00090121@uca.edu.sv>
2025-06-23 14:17:35 -0600

    adding changes

--- budget-buddy/src/js/pages/signIn.js
+++ budget-buddy/src/js/pages/signIn.js
@@ -13,11 +13,11 @@ export default function loadSigninPage() {
   <div class="w-full md:w-1/2 bg-gray-100 dark:bg-gray-900 flex items-center justify-center p-4 overflow-y-auto pt-6">
     <div class="w-full max-w-md">
       <!-- get back button-->
-      <a href="#" class="inline-flex items-center text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 mb-8">
+      <a href="../landing.html" class="inline-flex items-center text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 mb-8">
         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
         </svg>
-        Back to dashboard
+        Back to landing page
       </a>
 
       <!-- tittle -->
